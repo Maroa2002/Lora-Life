f3e9b46111f98534e94f1be5ef80cdbc58f6234e
Maroa2002 <mgm.engineeringtie847@gmail.com>
2025-02-18 06:49:52 +0300

    creates the route to the register page and renders its template

--- app.py
+++ app.py
@@ -12,10 +12,17 @@ def home():
 
     return render_template('index.html')
 
+
 @app.route('/login')
 def login():
 
     return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+
+    return render_template('register.html')
 
 if __name__ == '__main__':
     # with app.app_context():
