3fcd1e8af95157eb0b48924e5ed1721389d95ba4
Maroa2002 <mgm.engineeringtie847@gmail.com>
2025-02-18 06:45:12 +0300

    creates the route to the login page and renders its template

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def home():
 
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+
+    return render_template('login.html')
+
 if __name__ == '__main__':
     # with app.app_context():
     #     try:
