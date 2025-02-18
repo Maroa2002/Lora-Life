37dca67b1b825189f438383905303f1375be0581
Maroa2002 <mgm.engineeringtie847@gmail.com>
2025-02-18 06:32:07 +0300

    creates the route to the home page

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 from models import db, User, Farmer, Vet
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/vet_booking'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/vet_booking'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
+# db.init_app(app)
 
+@app.route('/')
+def home():
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    with app.app_context():
-        try:
-            db.create_all()
-            print("Database tables created successfully.")
-        except Exception as e:
-            print(f"Error creating tables: {e}")
+    # with app.app_context():
+    #     try:
+    #         db.create_all()
+    #         print("Database tables created successfully.")
+    #     except Exception as e:
+    #         print(f"Error creating tables: {e}")
 
     app.run(debug=True)
