ad56ef4e41892475e408307d167fdc29ed209b8c
Maroa2002 <mgm.engineeringtie847@gmail.com>
2025-02-18 08:34:30 +0300

    imported flask-migrate and initialized it for migrating the database

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from models import db, User, Farmer, Vet
 from dotenv import load_dotenv
 import os
+from flask_migrate import Migrate
 
 # load environment variables from .env
 load_dotenv()
@@ -19,6 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def home():
