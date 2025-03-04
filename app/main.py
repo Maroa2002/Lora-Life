dda423d76326cbd9f4749d0db15f3bcc218347ba
Maroa2002 <mgm.engineeringtie847@gmail.com>
2025-03-04 09:05:45 +0300

    defines the main routes for the Flask application

---
+++ app/main.py
@@ -0,0 +1,46 @@
+"""
+This module defines the main routes for the Flask application.
+
+It includes the following routes:
+- Home page
+- Contact form submission
+
+Functions:
+- home(): Renders the home page.
+- contact(): Handles the contact form submission and sends an email.
+"""
+
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from app.email_service import send_email
+
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def home():
+    """
+    Route to render the home page.
+
+    Returns:
+        Response: Rendered HTML template for the home page.
+    """
+    return render_template('index.html')
+
+@main_bp.route('/contact', methods=['POST'])
+def contact():
+    """
+    Route to handle contact form submission.
+
+    Processes the contact form data and sends an email with the provided information.
+
+    Returns:
+        Response: Redirects to the home page with a success message.
+    """
+    user_name = request.form.get('name')
+    user_email = request.form.get('email')
+    message = request.form.get('message')
+    
+    msg = "Subject: Contact Us\n\n{}\n\n{}\n\n{}".format(user_name, user_email, message)
+    
+    send_email(user_email, msg)
+    flash('Message sent successfully', 'success')
+    return redirect(url_for('main.home'))
