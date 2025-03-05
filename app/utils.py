91519ef0513bb9e43ebdcc9d93bc55f3b206c3aa
Maroa2002 <mgm.engineeringtie847@gmail.com>
2025-03-05 13:41:50 +0300

    imports email variables from the env file

--- app/utils.py
+++ app/utils.py
@@ -8,6 +8,14 @@ Functions:
 
 import smtplib
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
+# Fetch email credentials from environment variables
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
 ALLOWED_EXTENSIONS = {'pdf', 'doc', 'docx', 'jpg', 'jpeg', 'png'}
 
@@ -36,13 +44,11 @@ def send_email(recipient_email, msg):
         smtplib.SMTPException: If there is an error sending the email.
         Exception: If there is a general error.
     """
-    email_user = os.getenv("EMAIL_USER")
-    email_password = os.getenv("EMAIL_PASSWORD")
     try:
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
-        server.login(email_user, email_password)
-        server.sendmail(email_user, recipient_email, msg=msg)
+        server.login(EMAIL_USER, EMAIL_PASSWORD)
+        server.sendmail(EMAIL_USER, recipient_email, msg=msg)
         server.quit()
     except smtplib.SMTPException as e:
         print('Error sending email: {}'.format(e))
