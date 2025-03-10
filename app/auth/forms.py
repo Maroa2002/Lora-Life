d5221eeab80c1feb2e719a7eb23ac82d51853285
Maroa2002 <mgm.engineeringtie847@gmail.com>
2025-03-10 18:33:26 +0300

    user login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,14 @@
+"""
+This module defines the forms used for authentication and registration in the Flask application.
+
+It includes the following forms:
+- RoleSelectForm: Form to select user role.
+- BaseRegistrationForm: Base registration form for all user types.
+- FarmerRegistrationForm: Registration form for farmers.
+- VetRegistrationForm: Registration form for vets.
+- LoginForm: Form for user login.
+"""
+
 from flask_wtf import FlaskForm
 from wtforms import SelectField, IntegerField, SubmitField, StringField, FileField, TextAreaField, PasswordField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, NumberRange, InputRequired, Regexp
@@ -5,6 +16,13 @@ from flask_wtf.file import FileAllowed, FileRequired
 from app.utils import KENYA_COUNTIES
 
 class RoleSelectForm(FlaskForm):
+    """
+    Form to select user role.
+
+    Attributes:
+        user_role (SelectField): Field to select user role.
+        submit (SubmitField): Submit button for the form.
+    """
     user_role = SelectField(
         'Select Role:', # Label
         choices=[
@@ -17,12 +35,21 @@ class RoleSelectForm(FlaskForm):
     )
     submit = SubmitField('Submit')
 
-
 class BaseRegistrationForm(FlaskForm):
     """
     Base registration form for all user types.
+
+    Attributes:
+        first_name (StringField): Field for user's first name.
+        last_name (StringField): Field for user's last name.
+        email (StringField): Field for user's email.
+        phone (StringField): Field for user's phone number.
+        county (SelectField): Field for user's county.
+        town (SelectField): Field for user's town.
+        password (PasswordField): Field for user's password.
+        confirm_password (PasswordField): Field to confirm user's password.
+        profile_picture (FileField): Field for user's profile picture.
     """
-    
     first_name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=255)])
     last_name = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=255)])
     email = StringField('Email', validators=[DataRequired(), Email(), Length(max=255)])
@@ -49,6 +76,14 @@ class BaseRegistrationForm(FlaskForm):
 class FarmerRegistrationForm(BaseRegistrationForm):
     """
     Registration form for farmers.
+
+    Attributes:
+        farm_name (StringField): Field for farm name.
+        livestock_type (SelectField): Field for livestock type.
+        animal_count (IntegerField): Field for number of animals.
+        alert_preference (SelectField): Field for alert preference.
+        preferred_language (SelectField): Field for preferred language.
+        submit (SubmitField): Submit button for the form.
     """
     farm_name = StringField('Farm Name', validators=[Length(min=2, max=255)])
     livestock_type = SelectField(
@@ -99,6 +134,14 @@ class FarmerRegistrationForm(BaseRegistrationForm):
 class VetRegistrationForm(BaseRegistrationForm):
     """
     Registration form for vets.
+
+    Attributes:
+        license_number (StringField): Field for license number.
+        experience_years (IntegerField): Field for years of experience.
+        specialization (SelectField): Field for specialization.
+        clinic_name (StringField): Field for clinic name.
+        verification_document (FileField): Field for verification document.
+        submit (SubmitField): Submit button for the form.
     """
     license_number = StringField('License Number', validators=[DataRequired(), Length(min=5, max=255)])
     
@@ -135,4 +178,24 @@ class VetRegistrationForm(BaseRegistrationForm):
         )
     
     submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    """
+    Form for user login.
+
+    Attributes:
+        email (StringField): Field for user's email.
+        password (PasswordField): Field for user's password.
+    """
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=255)])
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            Length(min=8, max=255),
+            Regexp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
+                   message='Password must have at least one uppercase letter, one lowercase letter, one number, and one special character.')
+            ]
+        )
     
+    submit = SubmitField('Login')
