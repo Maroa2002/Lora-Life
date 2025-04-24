75831bb78bbfd7c30ea124841d84eb6a281cb459
Maroa2002 <mgm.engineeringtie847@gmail.com>
2025-04-24 11:12:59 +0000

    deleted the migrations/ folder and created a new one and creates the test_email.py file for email testing

---
+++ test_email.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = "mgm.engineeringtie847@gmail.com"
+EMAIL_PASSWORD = "awkbtddnggejrvvs"
+
+msg = EmailMessage()
+msg["Subject"] = "Test Email"
+msg["From"] = EMAIL_ADDRESS
+msg["To"] = "matikomaroa12@gmail.com"
+msg.set_content("This is a test from the server.")
+
+try:
+    with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+        smtp.starttls()
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
+    print("Email sent successfully")
+except Exception as e:
+    print("Error:", e)
